cc0bcef42c5f3c541214aeea1188b383d1f2d692
Sebastian Ortega <sebastian.ortega.fernandezz@gmail.com>
2023-06-01 03:25:49 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// Given an array of integers 'nums' and an integer 'target',
+// return indices of the two numbers such that they add up to target
+
+const twoSum = (nums, target) => {
+    const hashMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        // console.log(nums[i]);
+        const element = target - nums[i];
+        if (element in hashMap ) {
+            return [hashMap[element],i];
+        }
+        hashMap[nums[i]] = i;
+    }
+    
+}
+console.log(twoSum([2,1,5,3], 4));
